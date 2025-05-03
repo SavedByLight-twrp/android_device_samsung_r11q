1e63b66676038d31b2e02419ea5e3974217bb023
SavedByLight <mrfluffyoven@gmail.com>
2025-05-03 15:36:26 +0100

    remove anything that is no longer needed on device.mk

--- device.mk
+++ device.mk
@@ -15,18 +15,6 @@ PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd 
 
-#PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-
-# Health
-#PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
-
 # QCOM
 PRODUCT_PACKAGES += \
     qcom_decrypt \
