1dcab79724916e0e1f13a0e44db254e24fdc4204
Jamie Falconer <mrfluffyoven@gmail.com>
2025-05-04 16:44:26 +0100

    update orangefox flashlight path

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@
 export ALLOW_MISSING_DEPENDANCIES=true
 export TARGET_ARCH=arm64
 export OF_FLASHLIGHT_ENABLE=1
-export OF_FL_PATH1=
+export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
 export FOX_VERSION="Stable 1"
 export OF_MAINTAINER="Maxim_N_Jamie"
 export TARGET_DEVICE_ALT="r11q"
